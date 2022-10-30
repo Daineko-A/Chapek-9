965f282541afffc8a501c2d7e4e02662a12c028e
Alexandr Daineko <5056671@gmail.com>
2022-10-30 20:32:47 +0300

    commit

--- web/src/main/java/by/ploskiy/config/ConfigWeb.java
+++ web/src/main/java/by/ploskiy/config/ConfigWeb.java
@@ -1,16 +1,20 @@
 package by.ploskiy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @ComponentScan(basePackages = "by.ploskiy.controller")
 @EnableWebMvc
+@EnableSwagger2
 @Import(value = {ThymeleafConfig.class})
 public class ConfigWeb implements WebMvcConfigurer {
 
@@ -18,5 +22,30 @@ public class ConfigWeb implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         ResourceHandlerRegistration resourceRegistration = registry.addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addRedirectViewController(
+//                "/api/v2/api-docs", "/v2/api-docs");
+//        registry.addRedirectViewController(
+//                "/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+//        registry.addRedirectViewController(
+//                "/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+//        registry.addRedirectViewController(
+//                "/api/swagger-resources", "/swagger-resources");
+//    }
 }
